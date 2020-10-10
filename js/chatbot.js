0f6ebcf4018e400b405fba9ce270e2f2cc61b0dc
Rosa Virginia Encinas Quille <rosavirginiae@gmail.com>
2020-10-10 17:22:17 -0300

    Primera Versión

---
+++ js/chatbot.js
@@ -0,0 +1,10 @@
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+  document.getElementById("myForm").style.bottom = "90px";
+  document.getElementById("openb").style.display = "none";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+  document.getElementById("openb").style.display = "block";
+}
